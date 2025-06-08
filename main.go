c9fe4b90a8083917f33486662482c372b284b7e6
Alex Iero <a.ieronimos@gmail.com>
2025-06-08 13:13:31 +0100

    Add logic to enter and delete characters and change rows

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-var style = lipgloss.NewStyle().
+var defaultStyle = lipgloss.NewStyle().
 	Padding(0, 1).
 	Margin(0).
 	Border(lipgloss.RoundedBorder())
@@ -16,6 +16,8 @@ type grid []word
 
 type model struct {
 	grid grid
+	ri   int // row index
+	ci   int // column index
 }
 
 // write empty versions of init update and view functions required for our bubbletea model
@@ -30,6 +32,31 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c", "esc":
 			return m, tea.Quit
+		case "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z":
+			// if the key is a letter, add it to the grid at the current position
+			if m.ri < len(m.grid) && m.ci < len(m.grid[m.ri]) {
+				if len(msg.Runes) > 0 {
+					m.grid[m.ri][m.ci] = msg.Runes[0]
+					// move to the next column only if we're one position before the end
+					if m.ci < len(m.grid[m.ri])-1 {
+						m.ci++
+					}
+				}
+			}
+		case "enter":
+			// if the key is enter, move to the next row if we're one row before the end
+			if m.ri < len(m.grid)-1 {
+				m.ri++
+				m.ci = 0 // reset column index
+			}
+		case "backspace":
+			// if the key is backspace, remove the last character in the current row
+			if m.ci > 0 {
+				if m.grid[m.ri][m.ci] == ' ' {
+					m.ci--
+				}
+			}
+			m.grid[m.ri][m.ci] = ' '
 		}
 	}
 	return m, nil
@@ -41,7 +68,7 @@ func (m model) View() string {
 	for _, w := range m.grid {
 		row := []string{}
 		for _, r := range w {
-			row = append(row, style.Render(string(r)))
+			row = append(row, defaultStyle.Render(string(r)))
 		}
 		rows = append(rows, lipgloss.JoinHorizontal(lipgloss.Bottom, row...))
 	}
@@ -56,7 +83,7 @@ func main() {
 	for i := range grid {
 		grid[i] = []rune{' ', ' ', ' ', ' ', ' '}
 	}
-	grid[0] = []rune{'l', 'e', 'x', 'i', 's'}
+	// grid[0] = []rune{'l', 'e', 'x', 'i', 's'}
 	p := tea.NewProgram(model{grid: grid}, tea.WithAltScreen())
 
 	// run the program
