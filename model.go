75bbfbbc1e701429c8917b69c557b32e1a16fe72
ieroNo47 <a.ieronimos@gmail.com>
2026-03-29 15:08:10 +0100

    Check if answer is a valid word

--- model.go
+++ model.go
@@ -13,12 +13,19 @@ import (
 	"github.com/davecgh/go-spew/spew"
 )
 
+const (
+	stateLoading = iota
+	stateRowNotFull
+	stateInvalidWord
+	statePlaying
+)
+
 type model struct {
 	game           game
 	log            *log.Logger
 	help           help.Model
 	keys           keyMap
-	loading        bool
+	state          int
 	answerProvider answerProvider
 	spinner        spinner.Model
 }
@@ -27,6 +34,12 @@ type model struct {
 type initCompleteMsg struct {
 	answer string
 }
+
+// rowNotFullMsg is a message that is sent when the user tries to submit a guess but the current row is not full
+type rowNotFullMsg bool
+
+// invalidWordMsg is a message that is sent when the user tries to submit a guess but the word is not valid
+type invalidWordMsg bool
 
 // Init initializes the model and starts the game by getting the answer from the answer provider
 func (m model) Init() tea.Cmd {
@@ -50,7 +63,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case initCompleteMsg:
 		m.log.Debug("Initialization complete")
 		m.game.init(msg.answer)
-		m.loading = false
+		m.state = statePlaying
 		return m, nil
 	case tea.WindowSizeMsg:
 		oHorizontal := updateStyles(msg)
@@ -63,21 +76,43 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 		case key.Matches(msg, m.keys.Letter):
 			m.game.processLetter(msg.Text)
+			m.state = statePlaying
+			return m, nil
 		case key.Matches(msg, m.keys.Delete):
+			m.state = statePlaying
 			m.game.processDelete()
 		case key.Matches(msg, m.keys.Restart):
 			m.log.Info("==== Restarting game ====")
 			m.game.reset()
 			return m, nil
 		case key.Matches(msg, m.keys.Submit):
+			if m.game.rowReady() {
+				if m.answerProvider.validWord(m.game.rowString()) {
+					m.game.processSubmit()
+				} else {
+					return m, func() tea.Msg {
+						return invalidWordMsg(true)
+					}
+				}
+			} else {
+				return m, func() tea.Msg {
+					return rowNotFullMsg(true)
+				}
+			}
 			m.game.processSubmit()
 		}
 	case spinner.TickMsg:
-		if m.loading {
+		if m.state == stateLoading {
 			var cmd tea.Cmd
 			m.spinner, cmd = m.spinner.Update(msg)
 			return m, cmd
 		}
+	case rowNotFullMsg:
+		m.state = stateRowNotFull
+		return m, nil
+	case invalidWordMsg:
+		m.state = stateInvalidWord
+		return m, nil
 	default:
 		// if log level is debug, print the current string in the active row
 		if m.log.GetLevel() == log.DebugLevel {
@@ -101,8 +136,13 @@ func (m model) View() tea.View {
 	} else if m.game.isLost() {
 		resultS = fmt.Sprintf("Better luck next time! The answer was: %s", m.game.Answer())
 		resultRow = resultBarStyleLoss.Render(resultS)
-	} else if m.loading {
+	} else if m.state == stateLoading {
 		resultRow = resultBarStyleLoading.Render(m.spinner.View())
+	} else if m.state == stateRowNotFull {
+		resultRow = resultBarStyleError.Render("Row is not full!")
+	} else if m.state == stateInvalidWord {
+		resultRow = resultBarStyleError.Render("Invalid word!")
+
 	} else {
 		// debug row
 		resultS = fmt.Sprintf("Row: %d, Col: %d, RL: %d, L: %c, A: %s",
@@ -136,7 +176,7 @@ func newModel(logger *log.Logger) model {
 		log:            logger,
 		help:           newHelp(),
 		keys:           keys,
-		loading:        true,
+		state:          stateLoading,
 		answerProvider: provider,
 		spinner:        s,
 	}
